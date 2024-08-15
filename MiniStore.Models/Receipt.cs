7324c95e9f21db1f8574422ded1ae89720298eac
Akai <nguyensieu12112002@gmail.com>
2024-08-16 02:22:56 +0700

    Update Receipt.cs

--- MiniStore.Models/Receipt.cs
+++ MiniStore.Models/Receipt.cs
@@ -11,11 +11,13 @@ namespace MiniStore.Models
     public class Receipt
     {
         [Key]
-        public int Id { get; set; }
+        //public int Id { get; set; }
         [Column(name: "Receipt Id")]
         public string ReceiptId { set; get; } = string.Empty;
         [Column(name: "Supplier Id")]
+        [ForeignKey(nameof(Supplier))]
         public string SupplierID { get; set; } = string.Empty;
+        public Supplier Supplier { get; set; }
         [Column(name: "Create At")]
         public DateTime CreateAt { get; set; }
     }
