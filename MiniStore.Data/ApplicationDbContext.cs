943e55e772db4b459653879fd0a362615bd52c52
Akai <nguyensieu12112002@gmail.com>
2024-08-16 02:22:32 +0700

    Update ApplicationDbContext.cs

--- MiniStore.Data/ApplicationDbContext.cs
+++ MiniStore.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MiniStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,34 @@ namespace MiniStore.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly
+        public DbSet<Calender> Calenders { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<ReceiptDetail> ReceiptDetails { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<StyleItem> StyleItems { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=AKAI;Database=Noname;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;User Id = akai;Password=Akai123");
+
+
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
