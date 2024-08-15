2860a67683c2db03a21ce84377cef7781c6ec21f
Akai <nguyensieu12112002@gmail.com>
2024-08-16 02:22:58 +0700

    Update ReceiptDetail.cs

--- MiniStore.Models/ReceiptDetail.cs
+++ MiniStore.Models/ReceiptDetail.cs
@@ -8,14 +8,21 @@ using System.Threading.Tasks;
 
 namespace MiniStore.Models
 {
-    [Table(name:"Receip Detail")]
+    [Table(name:"Receipt Detail")]
     public class ReceiptDetail
     {
         [Key]
-        public int Id { get; set; }
+        //public int Id { get; set; }
         [Column(name: "Receipt Detail Id")]
         public string ReceiptDetailId { get; set; } = string.Empty;
+        
+        [Column(name: "Receipt Id")]
+        [ForeignKey(nameof(Receipt))]
+        public string ReceiptId { get; set; } = string.Empty;
+        public Receipt Receipt { get; set; }
+
         [ForeignKey(nameof(Item))]
+        [Column(name:"Item Id")]
         public string ItemId { get; set; } = string.Empty;
         public virtual Item Item { get; set; }
     }
