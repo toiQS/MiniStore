7e0bef37e05fae66c24dd468702d53c3e46fab0f
Akai <nguyensieu12112002@gmail.com>
2024-08-16 02:23:18 +0700

    Create Customer.cs

---
+++ MiniStore.Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniStore.Models
+{
+    public class Customer
+    {
+        [Key]
+        [Column(name:"Customer Id")]
+        public string CustomerId { get; set; } = string.Empty;
+        [Column(name:"Customer Name")]
+        public string CustomerName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public bool Status { get; set; }
+    }
+}
