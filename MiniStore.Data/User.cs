3cc8aa289ac15bdbc080c6c6d7592a076763ffab
Akai <nguyensieu12112002@gmail.com>
2024-08-16 02:23:10 +0700

    Create User.cs

---
+++ MiniStore.Data/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MiniStore.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniStore.Data
+{
+    public class User : IdentityUser
+    {
+        [ForeignKey(nameof(Employee))]
+        [Column(name:"Employee Id")]
+        public string EmployeeId { get; set; } = string.Empty;
+        public virtual Employee Employee { get; set; }
+    }
+}
