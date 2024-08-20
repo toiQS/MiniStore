72cb34277ab66efbfd7748d1d1f043d1605fb746
Akai <nguyensieu12112002@gmail.com>
2024-08-21 02:50:55 +0700

    Create CouponModelResponse.cs

---
+++ MiniStore.API/Models/coupon/CouponModelResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniStore.API.Models.coupon
+{
+    public class CouponModelResponse
+    {
+        public string CouponId { get; set; } = string.Empty;
+        public string CouponName { get; set; } = string.Empty;
+        public string CouponDescription { get; set; } = string.Empty;
+    }
+}
