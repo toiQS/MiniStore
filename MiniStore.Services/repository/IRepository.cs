de1e5d376738d0c9182ad172e7357072268fb793
Akai <nguyensieu12112002@gmail.com>
2024-08-21 02:51:39 +0700

    Create IRepository.cs

---
+++ MiniStore.Services/repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniStore.Services.repository
+{
+    public interface IRepository<T>
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetAsync(int id);
+        public Task<bool> Add(T entity);
+        public Task<bool> Delete(string id);
+        public Task<bool> Update(string id, T entity); 
+    }
+}
