f6933e9619c3f7d80bec3197250c934f4a222d82
Akai <nguyensieu12112002@gmail.com>
2024-08-21 02:51:35 +0700

    Create ICouponService.cs

---
+++ MiniStore.Services/coupon/ICouponService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualBasic;
+using MiniStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniStore.Services.coupon
+{
+    public interface ICouponService
+    {
+        public Task<IEnumerable<Coupon>> GetCouponsAsync();
+        //public Task<IEnumerable<Coupon>> GetCouponDetailById(string id);
+        //public Task<IEnumerable<Coupon>> GetCouponsByText(string text);
+        //public Task<IEnumerable<Coupon>> GetCouponsByDateRange(DateTime startAt , DateTime endAt);
+        //public Task<bool> AddCoupon (Coupon coupon);
+        //public Task<bool> EditCoupon(string couponId, Coupon coupon);
+        //public Task< bool> DeleteCoupon(string couponId);
+    }
+}
