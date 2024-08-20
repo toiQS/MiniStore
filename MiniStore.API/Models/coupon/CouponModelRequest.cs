ba5f6e099bebefed9c9091cc989d821c87e9f847
Akai <nguyensieu12112002@gmail.com>
2024-08-21 02:50:53 +0700

    Create CouponModelRequest.cs

---
+++ MiniStore.API/Models/coupon/CouponModelRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniStore.API.Models.coupon
+{
+    public class CouponModelRequest
+    {
+        public string CouponId { get; set; } = string.Empty;
+        public string CouponName { get; set; } = string.Empty;
+        public string CouponDescription { get; set; } = string.Empty;
+        public float Value { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+    }
+}
