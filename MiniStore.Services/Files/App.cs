7e000b6bdd4aace0742977eba81bc27c6a21737c
Akai <nguyensieu12112002@gmail.com>
2024-08-21 17:06:12 +0700

    Create App.cs

---
+++ MiniStore.Services/Files/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MiniStore.Services.Files
+{
+    public class App
+    {
+        public App() { }
+        /// <summary>
+        /// Gets the current folder.
+        /// </summary>
+        public string CurrentFolder()
+        {
+            string s = Assembly.GetCallingAssembly().Location;
+            return s.Remove(s.LastIndexOf('\\') + 1);
+        }
+        public string GetPathCurrentFolder(string folderName, string fileName)
+        {
+            var assemblyLocation = Assembly.GetEntryAssembly().Location;
+            var currentDirectory = Path.GetDirectoryName(assemblyLocation);
+            var appRootFolder = currentDirectory.Split("\\bin")[0];
+
+            string path = Path.Combine($"{appRootFolder}", $"{fileName}");
+            return path;
+            //return appRootFolder;
+        }
+
+    }
+}
+//"F:\\Repo\\Local\\MiniStore\\MiniStore.Services\\coupon\\ICouponService.cs"
+//F:\Repo\Local\MiniStore\MiniStore.Test\CouponUnitTest.cs
