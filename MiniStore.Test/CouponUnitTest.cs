a821436d82ebce4ab2b0f79c7c2d815b550b7589
Akai <nguyensieu12112002@gmail.com>
2024-08-21 17:06:15 +0700

    Create CouponUnitTest.cs

---
+++ MiniStore.Test/CouponUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using MiniStore.Data;
+using MiniStore.Services.coupon;
+using System.Security.Cryptography;
+
+namespace MiniStore.Test
+{
+    public class CouponUnitTest
+    {
+        private readonly ApplicationDbContext _context;
+        private ICouponService _couponService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _couponService = new CouponServices(_context);
+        }
+        [Test]
+        public void GetPath_AllUser()
+        {
+            var result =  _couponService.GetPath();
+            Console.WriteLine(result);
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
