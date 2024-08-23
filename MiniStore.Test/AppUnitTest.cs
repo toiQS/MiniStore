a0ff1906800f29f41b3c500c61d2825e2328bcca
Akai <nguyensieu12112002@gmail.com>
2024-08-24 02:35:07 +0700

    Create AppUnitTest.cs

---
+++ MiniStore.Test/AppUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using MiniStore.Services.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniStore.Test
+{
+    class AppUnitTest
+    {
+       private App _app;
+        [SetUp]
+        public void Setup()
+        {
+            _app = new App();
+        }
+        [Test]
+        public void Test()
+        {
+            var result = _app.GetPathCurrentFolder("folder1","file1");
+            Console.WriteLine(result);
+        }
+    }
+}
