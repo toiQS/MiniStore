9d818bfd221bd6f4d41d0f7ece8f45bb79b61526
Akai <nguyensieu12112002@gmail.com>
2024-08-23 18:08:17 +0700

    Create ReceiptController.cs

---
+++ MiniStore.API/Controllers/ReceiptController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniStore.Data;
+using MiniStore.Services.receipt;
+
+namespace MiniStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptController : ControllerBase
+    {
+        private readonly IReceiptServices _receiptServices;
+        public ReceiptController(IReceiptServices receiptServices)
+        {
+            _receiptServices = receiptServices;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetReceiptAsync()
+        {
+            var result = await _receiptServices.GetReceiptsAsync();
+            return Ok(result);
+        }
+    }
+}
