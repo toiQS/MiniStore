6716f50744dda1d02befa3c6b7d36a724ac05332
Akai <nguyensieu12112002@gmail.com>
2024-08-26 09:55:16 +0700

    add model

---
+++ MiniStore.API/Models/receipt/ReceiptModelResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniStore.API.Models.receipt
+{
+    public class ReceiptModelResponse
+    {
+        public string ReceiptId { set; get; } = string.Empty;
+        public string SupplierID { get; set; } = string.Empty;
+        public DateTime CreateAt { get; set; }
+    }
+}
