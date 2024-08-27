1ae67774de126abdab695ef4f80b4b8e4ec21a58
Akai <nguyensieu12112002@gmail.com>
2024-08-27 11:07:40 +0700

    update migration model receipt detail

--- MiniStore.Data/Migrations/20240826151646_Update-Model-ReceiptDetail.Designer.cs
+++ MiniStore.Data/Migrations/20240826151646_Update-Model-ReceiptDetail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniStore.Data;
 
@@ -11,9 +12,11 @@ using MiniStore.Data;
 namespace MiniStore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240826151646_Update-Model-ReceiptDetail")]
+    partial class UpdateModelReceiptDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MiniStore.Data/Migrations/20240826151646_Update-Model-ReceiptDetail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelReceiptDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Receipt Detail",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Receipt Detail");
+        }
+    }
+}
