4ce39929e7c968e2e2da41c5ea923c78bd026d3e
Akai <nguyensieu12112002@gmail.com>
2024-08-28 15:54:12 +0700

    delete model view, service and controller

--- MiniStore.API/Models/receipt/ReceiptModelRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniStore.API.Models.receipt
-{
-    public class ReceiptModelRequest
-    {
-        //public string ReceiptId { set; get; } = string.Empty;
-        public string SupplierID { get; set; } = string.Empty;
-        public DateTime CreateAt { get; set; }
-    }
-}
--- MiniStore.API/Models/receipt/ReceiptModelResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniStore.API.Models.receipt
-{
-    public class ReceiptModelResponse
-    {
-        public string ReceiptId { set; get; } = string.Empty;
-        public string SupplierID { get; set; } = string.Empty;
-        public DateTime CreateAt { get; set; }
-    }
-}
